21b29dec615a6c9a0f1d52d89f4e4ad88d1650ed
Guillaume ALLEGRET <gui.allegret@gmail.com>
2021-12-11 02:42:51 +0100

    github:pages

---
+++ precache-manifest.50fddbc56af634240f2c941a5b08e7c4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e341abbb1e35c1b90755",
+    "url": "/labels-generator/css/app.eb695203.css"
+  },
+  {
+    "revision": "9869bfcf9079ff40dce8cd1da73aa02c",
+    "url": "/labels-generator/fonts/Bafora Regular.9869bfcf.ttf"
+  },
+  {
+    "revision": "f5ea10663f048a928423054e36daf5d6",
+    "url": "/labels-generator/fonts/Bafora Regular.f5ea1066.otf"
+  },
+  {
+    "revision": "3c13a249f4b3200497bffa7875b297d1",
+    "url": "/labels-generator/fonts/Bondie.3c13a249.otf"
+  },
+  {
+    "revision": "90acd9adc52b94e11c2d20905a8a3a3b",
+    "url": "/labels-generator/fonts/Bondie.90acd9ad.ttf"
+  },
+  {
+    "revision": "d9b2058eb8286fe4c0ddb80847a9cb49",
+    "url": "/labels-generator/fonts/Futura-SC-T-OT-Book_19037.d9b2058e.ttf"
+  },
+  {
+    "revision": "8de9c2ca36efb295844914cdad72811d",
+    "url": "/labels-generator/img/flamme.8de9c2ca.svg"
+  },
+  {
+    "revision": "49aea8ef833f816da0ea40d7d695c108",
+    "url": "/labels-generator/index.html"
+  },
+  {
+    "revision": "e341abbb1e35c1b90755",
+    "url": "/labels-generator/js/app.a1724f38.js"
+  },
+  {
+    "revision": "7d44744aa5a2766dde2c",
+    "url": "/labels-generator/js/chunk-vendors.694bef21.js"
+  },
+  {
+    "revision": "d40202ff9b9f3406ca8f4095868e0ee1",
+    "url": "/labels-generator/manifest.json"
+  }
+]);
